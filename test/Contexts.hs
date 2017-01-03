1e1c90727075fd5ecc46f2ec5670c3e306223a15
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-01-03 20:18:00 +0100

    Import Data.Monoid

--- test/Contexts.hs
+++ test/Contexts.hs
@@ -3,11 +3,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Contexts where
 
-import NanoVG
-import           Language.C.Inline.HaskellIdentifier
-import qualified Language.C.Inline.Context as C
 import qualified Data.Map as M
+import           Data.Monoid
+import qualified Language.C.Inline.Context as C
+import           Language.C.Inline.HaskellIdentifier
 import qualified Language.C.Types as C
+import           NanoVG
 
 nanoVGCtx :: C.Context
 nanoVGCtx = mempty { C.ctxTypesTable = M.fromList [(C.TypeName "NVGcolor",[t|Color|])
