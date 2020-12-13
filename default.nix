e70deb3e6037127d1f71bfb93da43d1f588de43e
Richard Marko <srk@48.io>
2020-12-13 16:07:39 +0100

    Use FONS with freetype (-DFONS_USE_FREETYPE)

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.haskellPackages.callCabal2nixWithOptions "nanovg" ./. "-fexamples" {
   GLEW = null;
+  inherit (pkgs) freetype;
   inherit (pkgs) glew;
   inherit (pkgs) libGL;
   inherit (pkgs) libGLU;
