041be93b9bd1c6f17694b7898ff8439cfe18ec65
Richard Marko <srk@48.io>
2021-04-09 20:51:01 +0200

    X11, Nix files (#10)
    * Add X11 to extra-libraries
    
    Fixes
    
    * Missing (or bad) C library: X11
    
    * nix: switch to callCabal2nix, add shell.nix
    
    * Enable examples when using default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  haskellPkgs = pkgs.haskell.packages.ghc843.override(oldAttrs: {
-    overrides = self: super: {
-      nanovg = super.callPackage ./nanovg.nix {};
-    };
-  });
-in
-haskellPkgs.nanovg
+pkgs.haskellPackages.callCabal2nixWithOptions "nanovg" ./. "-fexamples" {
+  GLEW = null;
+  inherit (pkgs) glew;
+  inherit (pkgs) libGL;
+  inherit (pkgs) libGLU;
+  inherit (pkgs.xorg) libX11;
+}
